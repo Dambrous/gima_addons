0d9e7612fa01c2945d9bb72015cfb7a21f91d2cd
Dambrous <91986538+Dambrous@users.noreply.github.com>
2024-05-09 20:48:15 +0200

    [17][IMP][gima_partner] f-gas and iso portals t-if(if you have 0 fgas or 9001 you can't view the menus)

--- gima_partner/models/res_partner.py
+++ gima_partner/models/res_partner.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class ResPartner(models.Model):
@@ -6,6 +6,28 @@ class ResPartner(models.Model):
 
     gima_training_ids = fields.One2many("gima.training", "partner_id")
     gima_certifications_ids = fields.One2many("gima.certifications", "partner_id")
+    is_certified_fgas = fields.Boolean(string="Certified FGAS")
+    is_certified_iso_9001 = fields.Boolean(string="Certified ISO 9001")
+
+    @api.onchange('gima_certifications_ids')
+    def _onchange_certification(self):
+        for certification in self.gima_certifications_ids:
+            if certification.type in ['fgas_individual_licence', 'fgas_company_licence'] and certification.state in (
+            'valid', 'expiring'):
+                self.is_certified_fgas = True
+                break
+        else:
+            self.is_certified_fgas = False
+
+    @api.onchange('gima_certifications_ids')
+    def _onchange_certification(self):
+        for certification in self.gima_certifications_ids:
+            if certification.type == '9001_company_licence' and certification.state in (
+                    'valid', 'expiring'):
+                self.is_certified_iso_9001 = True
+                break
+        else:
+            self.is_certified_iso_9001 = False
 
     def action_view_certifications(self):
         certifications = self.env["gima.certifications"].search(
