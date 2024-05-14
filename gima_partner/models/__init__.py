9f47f53adc90153d5d74c534e7d7646d64024e09
Dambrous <91986538+Dambrous@users.noreply.github.com>
2024-05-15 01:33:40 +0200

    [FIX][gima_partner] fix import of dependencies of .py

--- gima_partner/models/__init__.py
+++ gima_partner/models/__init__.py
@@ -1,6 +1,5 @@
+from . import res_partner
 from . import gima_macro_course
 from . import gima_course
 from . import gima_training
 from . import gima_certifications
-from . import res_partner
-# from . import gima_course_line
--- gima_partner/models/gima_course_line.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models
-
-
-class GimaCourseLine(models.Model):
-    _name = "gima.course.line"
-
-    partner_id = fields.Many2one('res.partner', string='Partner', required=True)
-    course_id = fields.Many2one('gima.course', string='Course', required=True)
-
